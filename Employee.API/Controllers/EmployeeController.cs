4f2db225d841be6d3b433eff473e8452592431ef
Renu2811 <91143736+Renu2811@users.noreply.github.com>
2022-08-02 20:59:43 -0700

    Add files via upload

---
+++ Employee.API/Controllers/EmployeeController.cs
@@ -0,0 +1,123 @@
+﻿using Employee.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee.API.Controllers
+{
+
+
+    [ApiController]
+    [Route("api/employees")]
+
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeData _employeeData;
+        public EmployeeController(EmployeeData employeeData) => _employeeData = employeeData;
+
+        [HttpGet]
+        public ActionResult<IEnumerable<EmployeeViewModel>> GetEmployees()
+        {
+            return Ok(_employeeData.employeesList);
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+
+
+        [HttpPost]
+
+        public async Task<ActionResult<EmployeeViewModel>> Create(EmployeeViewModel employeeViewModel)
+        {
+            if (employeeViewModel == null)
+            {
+                return BadRequest();
+            }
+            _employeeData.employeesList.Add(employeeViewModel);
+            return Ok(_employeeData.employeesList);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, EmployeeViewModel employee)
+        {
+            if (employee == null)
+            {
+                return BadRequest("Employee object can't be null");
+            }
+            if (_employeeData.employeesList == null)
+            {
+                return NotFound("Table doesn't exists");
+            }
+            var employeeToUpdate = _employeeData.employeesList.Where(e => e.Id == id).FirstOrDefault();
+            if (employeeToUpdate == null)
+            {
+                return NotFound("Employee with this empId doesn't exists");
+            }
+            _employeeData.employeesList.Remove(employeeToUpdate);
+            employeeToUpdate.Id = employee.Id;
+            employeeToUpdate.FirstName = employee.FirstName;
+            employeeToUpdate.LastName = employee.LastName;
+            employeeToUpdate.MobileNumber = employee.MobileNumber;
+            employeeToUpdate.Email = employee.Email;
+            employeeToUpdate.AddressLine1 = employee.AddressLine1;
+            employeeToUpdate.AddressLine2 = employee.AddressLine2;
+            employeeToUpdate.PostalCode = employee.PostalCode;
+            employeeToUpdate.City = employee.City;
+            employeeToUpdate.Country = employee.Country;
+
+            _employeeData.employeesList.Add(employeeToUpdate);
+            return Ok(_employeeData.employeesList);
+        }
+
+
+
+
+
+
+
+
+
+
+
+        [HttpGet("{searchString}")]
+
+        public async Task<IActionResult> Show(string searchString)
+        {
+            if (searchString == null)
+            {
+                return BadRequest("input can't be null");
+            }
+            if (_employeeData.employeesList == null)
+            {
+                return NotFound("Table doesn't exists");
+            }
+            var employee =  _employeeData.employeesList.Where(e => e.FirstName.Contains(searchString) || e.LastName.Contains(searchString) || e.City.Contains(searchString) || e.Country.Contains(searchString)).ToList();
+            if (employee == null)
+            {
+                return NotFound("Record doesn't exists");
+            }
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int Id)
+        {
+            if (Id <= 0)
+            {
+                return BadRequest();
+            }
+            var employeeInDb = _employeeData.employeesList.FirstOrDefault(x => x.Id == Id);
+
+            if (employeeInDb == null)
+            {
+                return NotFound();
+            }
+
+            _employeeData.employeesList.Remove(employeeInDb);
+            //await _employeeData.SaveChangesAsync();
+            return Ok(_employeeData.employeesList);
+        }
+
+       
+    }
+}
---
+++ Employee.API/EmployeeData.cs
@@ -0,0 +1,77 @@
+﻿using Employee.API.Models;
+
+namespace Employee.API
+{
+    public class EmployeeData
+    {
+        public List<EmployeeViewModel> employeesList { get; set; }
+
+        public EmployeeData()
+        {
+            employeesList = new List<EmployeeViewModel>()
+            {
+                new EmployeeViewModel()
+                {
+                    Id = 1,
+                    FirstName = "Manasa",
+                    LastName = "Bhogiraju",
+                    MobileNumber = "9876543212",
+                    Email = "manasa@gmail.com",
+                    AddressLine1 = "Hno-12/3 Flat No-34",
+                    AddressLine2 = "Anjana Homes",
+                    City = "Hyderabad",
+                    PostalCode = "500070",
+                    Country = "India"
+                },
+                new EmployeeViewModel()
+                {
+                    Id = 2,
+                    FirstName = "Sneha",
+                    LastName = "Kadari",
+                    MobileNumber = "7989965432",
+                    Email = "sneha@gmail.com",
+                    AddressLine1 = "Hno-31/44 Flat No-20",
+                    AddressLine2 = "Bhanupriya Homes",
+                    City = "Banglore",
+                    PostalCode = "400908",
+                    Country = "India"
+                },
+                new EmployeeViewModel()
+                {
+                    Id = 3,
+                    FirstName = "Rishi",
+                    LastName = "Gowda",
+                    MobileNumber = "9876865443",
+                    Email = "rishi@gmail.com",
+                    AddressLine1 = "Hno-3/99 Flat No-30",
+                    AddressLine2 = "Manju Homes",
+                    City = "Chennai",
+                    PostalCode = "500943",
+                    Country = "India"
+                },
+                new EmployeeViewModel()
+                {
+                    Id = 4,
+                    FirstName = "Jhanu",
+                    LastName = "Thota",
+                    MobileNumber = "9876865356",
+                    Email = "jhanu@gmail.com",
+                    AddressLine1 = "Hno-7/99 Flat No-21",
+                    AddressLine2 = "Malthu Homes",
+                    City = "Delhi",
+                    PostalCode = "700989",
+                    Country = "India"
+                }
+
+            };
+        }
+
+        //internal Task SaveChanges()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
+
+
+
